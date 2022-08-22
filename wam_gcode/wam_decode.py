fadc3a8ce026f1cbf690649672cb406b145118f6
Allen Stevens <spdkils@hotmail.com>
2022-08-22 14:47:31 -0700

    Maybe fix nesting, and some refactor

--- wam_gcode/wam_decode.py
+++ wam_gcode/wam_decode.py
@@ -6,17 +6,14 @@ The one KNOWN bug is the fact it will probably fail to deal with 3 deep nested p
 (I haven't even tested that, I just fear it will blow up, or do something wrong.)
 
 
-I created this because after 8 months of use, I have run across many cuts that not being able to direct the cut order limits the machines capabilities.
+I created this because after 8 months of use, I have run across many cuts
+that the default cut order limits the machines capabilities.
 
 It has really three main use cases.
-    1)  Just preview the cut order VERY quickly and simply with a graphical interface. (Much easier for quick checks than NCViewer) (SAFE)
-    2)  It will allow you to manually select a cut, and move it up or down in the list of cuts, then let you save a COPY of the file with that change. (PROBABLY SAFE)
-    3)  It will find cuts that enclose other cuts, and ensure to cut them as a group, with the enclosing cut always last. (LEAST SAFE)
+    1)  Preview the cut order VERY quickly and simply with a graphical interface. (SAFE)
+    2)  Manually select a cut, and move it up or down in the list of cuts. (PROBABLY SAFE)
+    3)  Recursivly order cuts with the enclosing cut always last. (LEAST SAFE)
     ** Always check the file out on another utiliy, no guarentees!
-
-My hope is I don't have to maintain this thing for long, I hope that Wazer will add options to select differing cut orders, as well a
-cut order modification gui to their web tool. (I almost created this as a stand alone web app hosted on github using pyscript, and I may still do that
-just to learn how to do it.)
 
 """
 
@@ -48,11 +45,11 @@ BBox = namedtuple("BBox", "min_x, min_y, max_x, max_y")
 class Part:
     """Minimum data required to easily deal with g-code sections."""
 
-    points: tuple[float]
+    points: tuple[float] = field(repr=False)
     g_code: str = field(repr=False)
     bbox: BBox
     used: bool = False
-    parent: "Part" = None  # Used to deal with a small part that may get inside the rectangular bbox of another part.
+    children: "Part" = None
 
     def __hash__(self):
         return hash(self.g_code)
@@ -61,25 +58,25 @@ class Part:
 def read_file(filename: str) -> str:
     """Reads the provided file in UTF-8"""
     if Path(filename).is_file():
-        with open(filename, "r", encoding="utf-8") as f:
-            return f.read()
+        with open(filename, "r", encoding="utf-8") as f_handle:
+            return f_handle.read()
     return ""
 
 
-def a_encloses_b(a: BBox, b: BBox) -> bool:
+def a_encloses_b(bbox_a: BBox, bbox_b: BBox) -> bool:
     """Checks if the bounding box a, encloses bounding box b"""
     return (
-        a.min_x < b.min_x
-        and a.min_y < b.min_y
-        and a.max_x > b.max_x
-        and a.max_y > b.max_y
+        bbox_a.min_x <= bbox_b.min_x
+        and bbox_a.min_y <= bbox_b.min_y
+        and bbox_a.max_x >= bbox_b.max_x
+        and bbox_a.max_y >= bbox_b.max_y
     )
 
 
 def parts_by_row(parts: list[Part]):
     """Takes an iterable of parts, and sorts them into psudo rows"""
     rows = []
-    reversed = False
+    reverse_it = False
     new_parts = sorted(parts, key=lambda x: x.bbox.max_y, reverse=True)
     while new_parts:
         rows.append(
@@ -90,10 +87,10 @@ def parts_by_row(parts: list[Part]):
                     if new_parts[0].bbox.min_y < x.bbox.max_y or x == new_parts[0]
                 ],
                 key=lambda x: x.bbox.min_x,
-                reverse=reversed,
+                reverse=reverse_it,
             )
         )
-        reversed = not reversed
+        reverse_it = not reverse_it
         [new_parts.remove(x) for x in rows[-1]]
 
     return rows
@@ -110,15 +107,16 @@ def parse_gcode(gcode: str) -> tuple[str, str, list[Part]]:
 
     # hacked this up three times I should probably deal with the x/y as pairs in the regex
     # Meh...
+    regex = "(-?\\d+(?:\\.\\d+)?)"
     for match in re.finditer(
         BLOCK_START_REGEX + BLOCK_MIDDLE_REGEX + BLOCK_END_REGEX, gcode
     ):
-        x = [float(x) for x in re.findall("X(-?\\d+(?:\\.\\d+)?)", match.group(0))]
-        y = [float(y) for y in re.findall("Y(-?\\d+(?:\\.\\d+)?)", match.group(0))]
+        x_points = [float(x) for x in re.findall("X" + regex, match.group(0))]
+        y_points = [float(y) for y in re.findall("Y" + regex, match.group(0))]
         part = Part(
-            points=tuple((_x, _y) for _x, _y in zip(x, y)),
+            points=tuple((_x, _y) for _x, _y in zip(x_points, y_points)),
             g_code=match.group(0),
-            bbox=BBox(min(x), min(y), max(x), max(y)),
+            bbox=BBox(min(x_points), min(y_points), max(x_points), max(y_points)),
         )
         parts.append(part)
     if not all((header, footer, parts)):
@@ -144,18 +142,13 @@ def reorder_parts(parts: list[Part]) -> list[Part]:
     blocks = {}
     for part in parts:
         part.used = False
-        part.parent = None
+        part.children = []
     for part1 in parts:
         for part2 in parts:
             if part1 != part2 and a_encloses_b(part1.bbox, part2.bbox):
-                if part2.parent is None:
-                    part2.parent = part1
-                    part1.used = part2.used = True
-                    blocks.setdefault(part1, []).append(part2)
-                elif a_encloses_b(part1.bbox, part2.parent.bbox):
-                    part2.parent.parent = part1
-                    part1.used = part2.parent.used = True
-                    blocks.setdefault(part1, []).append(part2.parent)
+                part1.children.append(part2)
+                part1.used = part2.used = True
+                blocks.setdefault(part1, []).append(part2)
     for part in parts:
         if not part.used:
             blocks.setdefault(part, [])
@@ -165,22 +158,31 @@ def reorder_parts(parts: list[Part]) -> list[Part]:
     rows = parts_by_row(sorted_blocks)
     new_order = []
     for row in rows:
-        for item in row:
-            for child in blocks[item]:
-                new_order.append(child)
-            new_order.append(item)
+        for part in row:
+            extend = recursive_reorder(part)
+            for item in extend:
+                if item not in new_order:
+                    new_order.append(item)
     return new_order
+
+
+def recursive_reorder(part: Part):
+    """Reorder parts children first into a list"""
+    extension = []
+    for subpart in part.children:
+        extension.extend(recursive_reorder(subpart))
+    return extension + [part]
 
 
 def write_file(old_filename: Path, header: str, footer: str, parts: list[Part]):
     """Write the output file in the same folder as the input, adding a t_"""
     path = Path(old_filename)
     new_filename = path.parents[0] / f"t_{path.name}"
-    with open(new_filename, mode="w", encoding="utf-8") as f:
-        print(header, end="", file=f)
+    with open(new_filename, mode="w", encoding="utf-8") as file:
+        print(header, end="", file=file)
         for part in parts:
-            print(part.g_code, end="", file=f)
-        print(footer, end="", file=f)
+            print(part.g_code, end="", file=file)
+        print(footer, end="", file=file)
 
 
 def limit(num: int, min_allowed: int, max_allowed: int) -> int:
@@ -205,6 +207,7 @@ def draw_parts(
 
 
 def list_files(folder: str):
+    """Just list gcode files in supplied folder"""
     if Path(folder).is_dir():
         return [Path(x).name for x in glob.glob(folder + "/*.gcode")]
 
@@ -216,10 +219,9 @@ def main() -> int:
         int: Exit code for system.
     """
     sg.theme(choice(sg.theme_list()))
-    DRAW_COLOR = sg.theme_text_color()
 
     # Graph objects for drawings sorta like local globals, smells bad, but I'm in a hurry.
-    g1 = sg.Graph(
+    wazer_bed = sg.Graph(
         canvas_size=(CUT_WIDTH_MM, CUT_HEIGHT_MM),
         graph_bottom_left=(0, -CUT_HEIGHT_MM),
         graph_top_right=(CUT_WIDTH_MM, 0),
@@ -228,9 +230,8 @@ def main() -> int:
         enable_events=True,
         key="-raw_image-",
     )
-    list_box = []
-    lb = sg.Listbox(
-        values=list_box,
+    list_box = sg.Listbox(
+        values=[],
         key="Parts",
         size=(20, 30),
         select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE,
@@ -264,7 +265,7 @@ def main() -> int:
         ],
         [sg.Button("Up"), sg.Button("Down")],
         [slider],
-        [g1],
+        [wazer_bed],
         [sg.Text(text="", key="-metadata-")],
     ]
     right_col = [
@@ -278,7 +279,7 @@ def main() -> int:
             )
         ],
     ]
-    layout = [[lb, sg.Column(left_col), sg.Column(right_col)]]
+    layout = [[list_box, sg.Column(left_col), sg.Column(right_col)]]
 
     # GUI creation and execution loop.
 
@@ -304,25 +305,29 @@ def main() -> int:
 
             case ("-raw_image-", {"-raw_image-": pos}):
                 idxs = ()
-                for x in (-1, 0, 1):
-                    for y in (-1, 0, 1):
-                        idxs += g1.get_figures_at_location((pos[0] + x, pos[1] + y))
+                for x_wiggle in (-1, 0, 1):
+                    for y_wiggle in (-1, 0, 1):
+                        idxs += wazer_bed.get_figures_at_location(
+                            (pos[0] + x_wiggle, pos[1] + y_wiggle)
+                        )
                 locs = tuple(list(figure_mapping.values()).index(idx) for idx in idxs)
                 if locs:
                     window["Parts"].update(
-                        set_to_index=locs + lb.get_indexes(),
+                        set_to_index=locs + list_box.get_indexes(),
                         scroll_to_index=locs[0],
                     )
                 for idx in locs:
-                    g1.tk_canvas.itemconfig(figure_mapping[idx], fill="red")
+                    wazer_bed.tk_canvas.itemconfig(figure_mapping[idx], fill="red")
             case ("-animate-", {"-animate-": pos}):
                 for idx, fig in enumerate(window["Parts"].Values):
                     if idx < pos:
-                        g1.tk_canvas.itemconfig(figure_mapping[fig], fill="red")
+                        wazer_bed.tk_canvas.itemconfig(figure_mapping[fig], fill="red")
                     else:
-                        g1.tk_canvas.itemconfig(figure_mapping[fig], fill="white smoke")
+                        wazer_bed.tk_canvas.itemconfig(
+                            figure_mapping[fig], fill="white smoke"
+                        )
                 selected = tuple(x for x in range(0, int(pos)))
-                lb.update(set_to_index=selected, scroll_to_index=int(pos) - 1)
+                list_box.update(set_to_index=selected, scroll_to_index=int(pos) - 1)
             case ("Files", values):
                 if not values["Files"]:
                     continue
@@ -332,48 +337,52 @@ def main() -> int:
                     sg.popup("File did not parse correctly.")
                     continue
                 window["-metadata-"].update(value="\n".join(header.splitlines()[1:9]))
-                figure_mapping = draw_parts(parts, g1)
+                figure_mapping = draw_parts(parts, wazer_bed)
                 slider.update(range=(0, len(figure_mapping)))
                 slider.update(value=0)
-                lb.update(values=figure_mapping)
+                list_box.update(values=figure_mapping)
             case ("Re-Draw", values):
                 if not all((header, footer, parts)):
                     continue
-                figure_mapping = draw_parts(parts, g1)
+                figure_mapping = draw_parts(parts, wazer_bed)
                 slider.update(value=0)
-                lb.update(values=figure_mapping)
+                list_box.update(values=figure_mapping)
             case ("Rearrange", *_):
                 if not all((header, footer, parts)):
                     continue
                 parts = reorder_parts(parts)
-                figure_mapping = draw_parts(parts, g1)
+                figure_mapping = draw_parts(parts, wazer_bed)
                 slider.update(value=0)
-                lb.update(values=figure_mapping)
+                list_box.update(values=figure_mapping)
             case ("--REDRAW--", {"--REDRAW--": num}):
                 if not all((header, footer, parts)):
                     continue
-                figure_mapping = draw_parts(parts, g1)
+                figure_mapping = draw_parts(parts, wazer_bed)
                 new_ind = [
-                    limit(x + num, 0, len(lb.get_list_values()) - 1)
-                    for x in lb.get_indexes()
+                    limit(x + num, 0, len(list_box.get_list_values()) - 1)
+                    for x in list_box.get_indexes()
                 ]
                 scroll = 0 if not new_ind else new_ind[0]
-                lb.update(
+                list_box.update(
                     values=figure_mapping, set_to_index=new_ind, scroll_to_index=scroll
                 )
                 for fig in window["Parts"].Values:
-                    g1.tk_canvas.itemconfig(figure_mapping[fig], fill="white smoke")
+                    wazer_bed.tk_canvas.itemconfig(
+                        figure_mapping[fig], fill="white smoke"
+                    )
                 for fig in new_ind:
-                    g1.tk_canvas.itemconfig(figure_mapping[fig], fill="red")
+                    wazer_bed.tk_canvas.itemconfig(figure_mapping[fig], fill="red")
             case ("Parts", values):
                 for fig in window["Parts"].Values:
-                    g1.tk_canvas.itemconfig(figure_mapping[fig], fill="white smoke")
+                    wazer_bed.tk_canvas.itemconfig(
+                        figure_mapping[fig], fill="white smoke"
+                    )
                 for fig in values["Parts"]:
-                    g1.tk_canvas.itemconfig(figure_mapping[fig], fill="red")
+                    wazer_bed.tk_canvas.itemconfig(figure_mapping[fig], fill="red")
             case (event, values) if event in ("Up", "Down"):
                 val = -1 if event == "Up" else 1
                 if all((figure_mapping, parts)):
-                    for idx in lb.get_indexes()[::-val]:
+                    for idx in list_box.get_indexes()[::-val]:
                         if 0 <= idx + val < len(parts):
                             parts[idx + val], parts[idx] = parts[idx], parts[idx + val]
                     window.write_event_value("--REDRAW--", val)
